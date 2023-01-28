abe0e35c6ae3d455dac3e80ce392360c48498248
inovitskiy <drmnnml@gmail.com>
2023-01-28 10:08:33 +0300

    console colors, mock data

--- nodejs-devcamper-mcl/server.js
+++ nodejs-devcamper-mcl/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors')
 const connectDB = require('./config/db');
 
 //Route files
@@ -27,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT, 
-  console.log( `Server running in ${ process.env.NODE_ENV } mode on port ${PORT}` )
+  console.log( `Server running in ${ process.env.NODE_ENV } mode on port ${PORT}`.yellow.bold )
 );
 
 //Handle rejections
