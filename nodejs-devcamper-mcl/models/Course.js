39d964bef4013e025ce5d614bb85350494032169
inovitskiy <drmnnml@gmail.com>
2023-02-02 08:39:38 +0300

    average costs

--- nodejs-devcamper-mcl/models/Course.js
+++ nodejs-devcamper-mcl/models/Course.js
@@ -38,4 +38,38 @@ const CourseSchema = new mongoose.Schema({
   }
 });
 
+// Static method to get avg cost of course tuition
+CourseSchema.statics.updateAverageCost = async function (bootcampId) {
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' }
+      }
+    }
+  ])
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+    })
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Call getAverageCost after save
+CourseSchema.post('save', function () {
+  this.constructor.updateAverageCost(this.bootcamp);
+})
+
+// Call getAverageCost before remove
+CourseSchema.pre('remove', function () {
+  this.constructor.updateAverageCost(this.bootcamp);
+})
+
 module.exports = mongoose.model('Course',CourseSchema);
