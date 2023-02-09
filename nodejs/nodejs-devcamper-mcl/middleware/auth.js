6524e26a22ad789926078ab1ee8e74b30508f68a
inovitskiy <drmnnml@gmail.com>
2023-02-09 06:21:42 +0300

    roles handler

--- nodejs/nodejs-devcamper-mcl/middleware/auth.js
+++ nodejs/nodejs-devcamper-mcl/middleware/auth.js
@@ -39,3 +39,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Not authorize to access this route', 401));
   }
 })
+
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access this route`, 401));
+    }
+    next();
+  }
+}
