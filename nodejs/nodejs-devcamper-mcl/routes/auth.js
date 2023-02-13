294b1374d4de8160545512e628f2291d6905ebf7
inovitskiy <drmnnml@gmail.com>
2023-02-13 09:14:20 +0300

    update user details and password

--- nodejs/nodejs-devcamper-mcl/routes/auth.js
+++ nodejs/nodejs-devcamper-mcl/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
+const { register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -9,6 +9,8 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/me').get(protect, getMe);
 router.route('/forgotpassword').post(forgotPassword);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 // todo: setup SMTP
 router.put('/resetpassword/:resettoken', resetPassword);
 
