96e00a6f1e817aa87a0c736d1972a74a4c8f1267
inovitskiy <drmnnml@gmail.com>
2023-02-15 06:30:25 +0300

    reviews model/get

--- nodejs/nodejs-devcamper-mcl/routes/bootcamps.js
+++ nodejs/nodejs-devcamper-mcl/routes/bootcamps.js
@@ -15,6 +15,7 @@ const advancedResults = require('../middleware/advancedResults');
 
 // Include other resourse routers
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 const router = express.Router();
 
@@ -22,16 +23,14 @@ const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resourse routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
-
 router.route('/:id/photo').put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
-
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
   .post(protect,authorize('publisher', 'admin'),createBootcamp)
-
 router
   .route('/:id')
   .get(getBootcamp)
