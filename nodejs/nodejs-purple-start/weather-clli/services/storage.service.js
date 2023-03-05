11d57641796f17f0a7daa73c75e4b17350bd93be
inovitskiy <drmnnml@gmail.com>
2023-03-05 14:34:09 +0300

    os and path

--- nodejs/nodejs-purple-start/weather-clli/services/storage.service.js
+++ nodejs/nodejs-purple-start/weather-clli/services/storage.service.js
@@ -1,9 +1,36 @@
 import { homedir } from 'os';
 import { join } from 'path'; // basename, dirname, extname, relative, isAbsolute, resove, sep
+import { promises } from 'fs';
 
-const filePath = console.log(join(homedir(), 'weather-data.json'));
+const filePath = join(homedir(), 'weather-data.json');
 
-const saveKeyValue = (key, value) => {
+const saveKeyValue = async (key, value) => {
+  let data = {};
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    data = JSON.parse(file);
+  };
+  data[key] = value;
+  await promises.writeFile(filePath, JSON.stringify(data));
 };
+
+const getKeyValue = async (key) => {
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    const data = JSON.parse(file);
+    return data[key];
+  }
+   return undefined;
+};
+
+const isExist = async (path) => {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch(e) {
+    return false;
+  }
+  
+}
 
 export {saveKeyValue};
--- nodejs/nodejs-purple-start/weather-clli/weather.js
+++ nodejs/nodejs-purple-start/weather-clli/weather.js
@@ -3,6 +3,15 @@ import { getArgs } from  './helpers/args.js';
 import { printError, printSuccess, printHelp } from './services/log.service.js';
 import { saveKeyValue } from './services/storage.service.js';
 
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', args.t);  
+    printSuccess('Токен сохранен');
+  } catch (e) {
+    printError(e.message);
+  }
+}
+
 const initCLI = () => {
   const args = getArgs(process.argv);
   if (args.h) {
