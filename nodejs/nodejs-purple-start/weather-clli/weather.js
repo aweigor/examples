293ec2de94665093b1ae1509c39066c9eb031705
inovitskiy <drmnnml@gmail.com>
2023-03-06 07:41:53 +0300

    api call beta

--- nodejs/nodejs-purple-start/weather-clli/weather.js
+++ nodejs/nodejs-purple-start/weather-clli/weather.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from  './helpers/args.js';
+import { getWeather } from './services/api.service.js';
 import { printError, printSuccess, printHelp } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
+  if (!token.length) {
+    printError('не передан token');
+    return;
+  }
   try {
-    await saveKeyValue('token', args.t);  
+    await saveKeyValue(TOKEN_DICTIONARY.token, args.t);  
     printSuccess('Токен сохранен');
   } catch (e) {
     printError(e.message);
@@ -23,6 +28,7 @@ const initCLI = () => {
   if (args.t) {
     saveKeyValue('token', args.t);
   }
+  getWeather('moscow')
   // weather
 };
 
