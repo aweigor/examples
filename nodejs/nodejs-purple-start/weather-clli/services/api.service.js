d349e12ff6bc605943ca4958bb6030b1ae6dbb09
inovitskiy <drmnnml@gmail.com>
2023-03-07 09:24:25 +0300

    api response debug

--- nodejs/nodejs-purple-start/weather-clli/services/api.service.js
+++ nodejs/nodejs-purple-start/weather-clli/services/api.service.js
@@ -1,27 +1,22 @@
+import axios from 'axios';
 import https from 'https';
-import { getKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
 const getWeather = async (city) => {
   const token = await getKeyValue(TOKEN_DICTIONARY.token);
   if (!token) {
     throw new Error('Не задан ключ api');
   }
-  //const url = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${key}`;
-  const  url = new URL('https://api.openweathermap.org/data/3.0/onecall');
-  url.searchParams.append('lat', -94.04);
-  url.searchParams.append('exclude', 'hourly,daily');
-  url.searchParams.append('appid',token);
 
-  https.get(url, (response) => {
-    let res = '';
-    response.on('data', (chunk) => {
-      res += chunk;
-    });
-
-    response.on('end', () => {
-      console.log(res);
-    });
+  const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+    params: {
+      q: city,
+      appid: token,
+      lang: 'ru',
+      units: 'metric'
+    }
   });
+  console.log(data);
 };
 
 export { getWeather };
