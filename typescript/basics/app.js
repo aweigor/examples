0a96fb2d959e97269569f1886ec645bc09de4837
inovitskiy <drmnnml@gmail.com>
2023-03-14 21:02:46 +0300

    basic typs, object typing

--- typescript/basics/app.js
+++ typescript/basics/app.js
@@ -1,6 +1,9 @@
 "use strict";
 exports.__esModule = true;
-var revenue = 1000;
-var bonus = 500;
-var res = revenue + bonus;
-console.log(res);
+function getFullName(firstname, surname) {
+    return "".concat(firstname, " ").concat(surname);
+}
+var getFullYearArrow = function (firstname, surname) {
+    return "".concat(firstname, " ").concat(surname);
+};
+console.log(getFullYearArrow('hey', 'hoe'));
--- typescript/basics/app.ts
+++ typescript/basics/app.ts
@@ -1,7 +1,23 @@
-let revenue: number = 1000;
-let bonus: number = 500;
+function getFullName(userEntity: { firstname: string, surname: string }) {
+  return `${userEntity.firstname} ${userEntity.surname}`;
+}
 
-let res: number = revenue + bonus;
-console.log(res);
+const getFullYearArrow = (firstname: string, surname: string): string => {
+  return `${firstname} ${surname}`;
+}
+
+const user = {
+  firstname: 'John',
+  surname: 'Doe',
+  city: 'Kasablanca',
+  age: 30,
+  skills: {
+    dev: true,
+    devops: true
+  }
+}
+
+console.log(getFullYearArrow('hey', 'hoe'));
+console.log(getFullName(user));
 
 export {};
