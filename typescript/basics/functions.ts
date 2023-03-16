ef0db5c189dbed4025c889fc06a05c83bcddff5f
inovitskiy <drmnnml@gmail.com>
2023-03-16 20:33:51 +0300

    readonly, enums

--- typescript/basics/functions.ts
+++ typescript/basics/functions.ts
@@ -17,7 +17,34 @@ const skill: [number, string] = [1, 'Dev'];
 const id = skill[0];
 const skillName = skill[1]; // index 2 not valid
 
+// Readonly
+const roparams: readonly string[] = ['Fooo', 'Bars'];
+
 // spread typings
 const arr: [number, string, ...boolean[]] = [1, 'asd', true, false];
+
+
+// enums
+// by default - index of array
+enum StatusCode {
+  SUCCESS = 1,
+  IN_PROCESS = 'in_process',
+  FAILED = 2
+}
+
+const enumres = {
+  message: '',
+  statusCode: StatusCode.SUCCESS
+}
+
+if (enumres.statusCode === StatusCode.IN_PROCESS) {
+  //...
+}
+
+// contant enum, replaces value with given
+const enum Roles {
+  ADMIN = 1,
+  USER = 2
+}
 
 console.log(res);
