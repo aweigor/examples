4d15fbe2ac2834d2a8bae15344f9e8b2e1345b0d
inovitskiy <drmnnml@gmail.com>
2023-03-28 20:47:59 +0300

    typeguard

---
+++ typescript/basics/guard.ts
@@ -0,0 +1,44 @@
+interface User {
+  name: string;
+  email: string;
+  login: string;
+}
+
+const user: User = {
+  name: 'Вася',
+  email: 'vasiliy@yandex.ru',
+  login: 'vasia'
+}
+
+interface Admin {
+  name: string;ß
+  role: number;
+}
+
+function logId(id: string | number) {
+  if (isString(id)) {
+    console.log(id);
+  } else {
+    console.log(id);
+  }
+}
+
+function isString(x: string | number): x is string {
+  return typeof x === 'string';
+}
+
+function isAdmin(user: User | Admin): user is Admin {
+  return 'role' in user;
+}
+
+function isAdminAlternative(user: User | Admin): user is Admin {
+  return (user as Admin).role !== undefined;
+}
+
+function setRoleZero(user: User | Admin) {
+  if (isAdmin(user)) {
+    user.role = 0;
+  } else {
+    throw new Error('Пользователь не админ');
+  }
+}
