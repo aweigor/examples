1a83a2c88ae8226c2b2072fff0e3b5fe0fd97ca1
inovitskiy <drmnnml@gmail.com>
2023-04-07 21:54:47 +0300

    static methods addnl

--- typescript/classes/static.ts
+++ typescript/classes/static.ts
@@ -2,8 +2,23 @@ class UserService {
   private static db: any;
 
   static getUser(id: number) {
-    return this.db.findById(id);
+    return UserService.db.findById(id);
+  }
+
+  constructor(id: number) {}
+
+  create() {
+    UserService.db;
+  }
+
+  static {
+    UserService.db = 'asd';
   }
 }
 
 UserService.getUser(1);
+
+const inst = new UserService(1);
+
+// static methods alloud inside of non-static methods
+inst.create();
