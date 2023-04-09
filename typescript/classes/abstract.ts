d2f4703dfa34cb75c5e87ff900e3b4e1af7191a6
inovitskiy <drmnnml@gmail.com>
2023-04-09 19:59:48 +0300

    abstract excercise

--- typescript/classes/abstract.ts
+++ typescript/classes/abstract.ts
@@ -16,3 +16,23 @@ class UserController extends Controller {
 
 const c = new UserController();
 c.handleWithLogs('Request');
+
+
+// excercise
+abstract class Logger {
+  abstract log(message);
+
+  printDate(date: Date) {
+    this.log(date.toString());
+  }
+}
+
+class SimpleLogger extends Logger {
+  log(message: string): void {
+    console.log(message);
+  }
+  logWithDate(message: string) {
+    this.printDate(new Date());
+    this.log(message);
+  }
+}
