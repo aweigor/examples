224a8be57435d3d61234363ee39a4fc87a59ca6d
inovitskiy <drmnnml@gmail.com>
2023-04-18 20:47:50 +0300

    class generics

---
+++ typescript/generics/classes.ts
@@ -0,0 +1,24 @@
+class Resp<D, E> {
+  data?: D;
+  error?: E;
+
+  constructor(data?: D, error?: E) {
+    if (data) {
+      this.data = data;
+    }
+    if (error) {
+      this.error = error;
+    }
+  }
+}
+
+const res = new Resp<string, number>('data');
+
+class HTTPResp extends Resp<string, number> {
+  code: number;
+  setCode(code: number) {
+    this.code = code;
+  }
+}
+
+const res2 = new HTTPResp();
