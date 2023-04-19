1b0cf21503fe845e1bec28d305d1145837d695b6
inovitskiy <drmnnml@gmail.com>
2023-04-19 22:19:43 +0300

    type modifications keyof

---
+++ typescript/typemod/keyof.ts
@@ -0,0 +1,19 @@
+interface IUser {
+  name: String;
+  age: Number;
+}
+
+type KeysOfUser = keyof IUser;
+
+const key: KeysOfUser = 'age';
+
+function getValue<T, K extends keyof T>(obj: T, key: K){
+  return obj[key];
+}
+
+const user: IUser = {
+  name: 'Вася',
+  age: 30
+}
+
+const userName = getValue(user, 'names');
