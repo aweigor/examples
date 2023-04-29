c3665528fd2c41728eb12ce26ea7eba07030d07b
inovitskiy <drmnnml@gmail.com>
2023-04-29 22:55:14 +0300

    partial

---
+++ typescript/servicetypes/partial.ts
@@ -0,0 +1,12 @@
+interface User {
+  name: string;
+  age?: number;
+  email?: string;
+}
+
+type partial = Partial<User>;
+const p: partial = {};
+
+type required = Required<User>;
+type readonly = Readonly<User>
+type requiredAndReadonly = Required<Readonly<User>>;
