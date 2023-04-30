dd7c1651add81f565de1b451b5f4d9b05bc0d1c9
inovitskiy <drmnnml@gmail.com>
2023-04-30 23:04:36 +0300

    omit / pick

---
+++ typescript/servicetypes/omit.ts
@@ -0,0 +1,12 @@
+interface PaymentPersistent {
+  id: number;
+  sum: number;
+  from: string;
+  to: string;
+}
+
+type Payment = Omit<PaymentPersistent, 'id'>;
+type PaymentRequisits = Pick<PaymentPersistent, 'from' | 'to'>;
+
+type ExtractEx = Extract<'from' | 'to' | Payment, string>;
+type ExcludeEx = Extract<'from' | 'to' | Payment, string>;
