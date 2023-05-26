787b73e6c10225252b650499710be3fd7c689b5b
inovitskiy <drmnnml@gmail.com>
2023-05-26 23:29:15 +0300

    ts strategy pattern

---
+++ typescript/patterns/actions/strategy.ts
@@ -0,0 +1,46 @@
+class User {
+  githubToken: string;
+  jwtToken: string;
+}
+
+interface AuthStrategy {
+  auth(user: User): boolean;
+}
+
+class Auth {
+  constructor(private strategy: AuthStrategy) {}
+  setStrategy(strategy: AuthStrategy) {
+    this.strategy = strategy;
+  }
+  public authUser(user: User): boolean {
+    return this.strategy.auth(user);
+  }
+}
+
+class JWTStrategy implements AuthStrategy {
+  auth(user: User): boolean {
+    if (user.jwtToken) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class GithubStrategy implements AuthStrategy {
+  auth(user: User): boolean {
+    if (user.githubToken) {
+      return true;
+    }
+    return false;
+  }
+}
+
+const user = new User();
+user.jwtToken = 'token';
+user.githubToken = 'token';
+
+const auth = new Auth(new JWTStrategy());
+console.log(auth.authUser(user));
+auth.setStrategy(new GithubStrategy())
+
+export {};
