ce86d35664dcfbba279b6d093ac3cf54d2f106ae
inovitskiy <drmnnml@gmail.com>
2023-06-19 20:36:38 +0300

    ts node exception filter

--- nodejs/nodejs-purple-start/dashboard-api/src/app.ts
+++ nodejs/nodejs-purple-start/dashboard-api/src/app.ts
@@ -3,27 +3,34 @@ import { userRouter } from './users/users';
 import { Server } from 'node:http';
 import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/users.controller';
-
+import { ExceptionFilter } from './errors/exception.filter';
 
 export class App {
   app: Express;
   server?: Server;
   port: number;
   logger: LoggerService;
-  userController: UserController
+  userController: UserController;
+  exceptionFilter: ExceptionFilter;
   
   constructor(
     logger: LoggerService,
-    userController: UserController
+    userController: UserController,
+    exceptionFilter: ExceptionFilter
     ) {
     this.app = express();
     this.port = 8000;
     this.logger = logger;
     this.userController = userController;
+    this.exceptionFilter = exceptionFilter;
   }
 
   useRoutes() {
-    this.app.use('/users', userRouter);
+    this.app.use('/users', this.userController.router);
+  }
+
+  useExceptionFilters() {
+    this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
   }
 
   public async init() {
