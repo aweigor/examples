1230edd3a18dd006b55a2d104529240aadabaea0
inovitskiy <drmnnml@gmail.com>
2023-06-22 23:47:32 +0300

    x

--- nodejs/nodejs-purple-start/dashboard-api/src/test2.ts
+++ nodejs/nodejs-purple-start/dashboard-api/src/test2.ts
@@ -1,5 +1,30 @@
 import 'reflect-metadata';
 
-function Test(target: Function) {
-  
+function Injectable(key: string) {
+  return (target: Function) => {
+    Reflect.defineProperty(target, key, 1);
+    const meta = Reflect.getOwnPropertyDescriptor(target, key);
+    console.log(meta);
+  }
+}
+
+
+function Inject(key: string) {
+  return (target: Function) => {
+    Reflect.defineProperty(target, key, 1);
+    const meta = Reflect.getOwnPropertyDescriptor(target, key);
+    console.log(meta);
+  }
+}
+
+function Prop(target: Object, neme: string) {}
+
+@Injectable('C')
+export class C {
+  @Prop prop?: number;
+}
+
+@Injectable('D')
+export class D {
+  constructor(@Inject('C') c: C) {}
 }
