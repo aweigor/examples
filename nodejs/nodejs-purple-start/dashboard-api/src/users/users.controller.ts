89a4462dab9fa99ed527d810a702426aecc4cb85
inovitskiy <drmnnml@gmail.com>
2023-06-25 21:10:28 +0300

    ts nodejs bootstrap

--- nodejs/nodejs-purple-start/dashboard-api/src/users/users.controller.ts
+++ nodejs/nodejs-purple-start/dashboard-api/src/users/users.controller.ts
@@ -5,9 +5,10 @@ import { HTTPError } from "../errors/http-error.class";
 import { ILogger } from "../logger/logger.interface";
 import { inject, injectable } from 'inversify';
 import { TYPES } from "../types";
+import { IUserController } from "./users.controller.interface";
 
 @injectable()
-export class UserController extends BaseController {
+export class UserController extends BaseController implements IUserController {
   constructor(@inject(TYPES.ILogger) private loggerService: ILogger) 
   {
     super(loggerService);
