7be3a1d3a03d6b022d1ab44bf4a160be3df3f8e3
inovitskiy <drmnnml@gmail.com>
2023-07-11 20:20:04 +0300

    ts node close function

--- nodejs/nodejs-purple-start/dashboard-api/src/main.ts
+++ nodejs/nodejs-purple-start/dashboard-api/src/main.ts
@@ -31,12 +31,12 @@ export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<App>(TYPES.Application).to(App).inSingletonScope();
 });
 
-function bootstrap(): BootstrapReturn {
+async function bootstrap(): Promise<BootstrapReturn> {
 	const appContainer = new Container();
 	appContainer.load(appBindings);
 	const app = appContainer.get<App>(TYPES.Application);
-	app.init();
+	await app.init();
 	return { appContainer, app };
 }
 
-export const { app, appContainer } = bootstrap();
+export const boot = bootstrap();
