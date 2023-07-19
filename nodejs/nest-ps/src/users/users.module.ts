3983f039b060343cc6524ccb9249cd42de52dced
inovitskiy <drmnnml@gmail.com>
2023-07-19 20:52:29 +0300

    models relations

--- nodejs/nest-ps/src/users/users.module.ts
+++ nodejs/nest-ps/src/users/users.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Post, PostSchema } from './models/post.model';
+import { User, UserSchema } from './models/user.model';
 
-@Module({})
+@Module({
+	providers: [UsersModule],
+	imports: [
+		MongooseModule.forFeature([
+			{ name: User.name, schema: UserSchema },
+			{ name: Post.name, schema: PostSchema },
+		])
+	]
+})
 export class UsersModule {}
