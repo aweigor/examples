430ce1b7dd4e248d1607969750c49d49f09a6247
inovitskiy <drmnnml@gmail.com>
2023-07-20 21:40:25 +0300

    mongodb options

--- nodejs/nest-ps/src/app.module.ts
+++ nodejs/nest-ps/src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig
+    }),
     UsersModule
   ],
   controllers: [AppController],
