90ccb48b123dd201be80a3bda6647a3aa094a7a5
inovitskiy <drmnnml@gmail.com>
2023-08-04 20:11:18 +0300

    nest jwt auth

--- nodejs/nest-ps/src/auth/auth.constants.ts
+++ nodejs/nest-ps/src/auth/auth.constants.ts
@@ -1,1 +1,3 @@
 export const ALREADY_REGISTERED = 'Такой пользователь уже зарегистрирован';
+export const USER_NOT_FOUND_ERROR = 'Пользователь с таки email не найден';
+export const WRONG_PASSWORD = 'Неверный пароль';
