17fb9fc4e10fa00fc32bbd00c65f6a7cdae21744
inovitskiy <drmnnml@gmail.com>
2023-08-14 16:44:04 +0300

    nest app

--- nodejs/nest-ps/src/auth/auth.constants.ts
+++
@@ -1,3 +0,0 @@
-export const ALREADY_REGISTERED = 'Такой пользователь уже зарегистрирован';
-export const USER_NOT_FOUND_ERROR = 'Пользователь с таки email не найден';
-export const WRONG_PASSWORD = 'Неверный пароль';
