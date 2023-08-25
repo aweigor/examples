e658750e4fa2efe0db36a6c709bd11ac5ec77255
inovitskiy <drmnnml@gmail.com>
2023-08-25 19:27:54 +0300

    metadata

--- nextjs/first-next-app/app/page.tsx
+++ nextjs/first-next-app/app/page.tsx
@@ -1,5 +1,20 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "MyTitle"
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+  // ...
+  return {
+    title: 'ComputedMeta',
+    icons: {
+      icon: '/icon.ico'
+    }
+  };
+}
 
 export default function Home() {
   const a = 1;
